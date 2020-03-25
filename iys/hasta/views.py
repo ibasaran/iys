81a22c250a6309f97e8fd3d761fba01fc82467f4
İsmail BAŞARAN <basaran.ismaill@gmail.com>
2020-03-25 14:59:49 +0300

    etiketi hasta soyadı eklendi

--- iys/hasta/views.py
+++ iys/hasta/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 
 def hastaList(request):
 
-    if (request.user.username == 'erkan'):
+    if (request.user.username == 'erkan' or request.user.username == 'aysel'):
         return render(request, 'hasta/list.html', {'hastalar':Hasta.objects.all()})
 
     hastaneId = request.session['hastaneId']
